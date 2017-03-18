a3a9922cc0b98ae4dc90791b234d4d3b3ee6c9da
Don Knapp <don@dknapp.com>
2017-03-18 18:37:03 -0400

    I've added the sequelize  work for the tables. datatest.js validates the operations of the sequelize functionality.

---
+++ app/models/schedule_dtl.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var schedule_dtl = sequelize.define('schedule_dtl', {
+    schedule_id: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    date: DataTypes.DATEONLY,
+    hour: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  schedule_dtl.sync();
+  return schedule_dtl;
+};
