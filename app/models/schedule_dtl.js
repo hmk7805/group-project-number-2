93796a7d1cb3e4da14c82c3a9cd6ffa29d2ad252
Don Knapp <don@dknapp.com>
2017-03-22 13:29:06 -0400

    Added views for extraction and added missing column to schedule_dtls

--- app/models/schedule_dtl.js
+++ app/models/schedule_dtl.js
@@ -3,6 +3,7 @@ module.exports = function(sequelize, DataTypes) {
   var schedule_dtl = sequelize.define('schedule_dtl', {
     schedule_id: DataTypes.INTEGER,
     type: DataTypes.STRING,
+    user_id: DataTypes.INTEGER,
     date: DataTypes.DATEONLY,
     hour: DataTypes.INTEGER
   }, {
