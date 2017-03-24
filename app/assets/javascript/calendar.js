3fc8b0ba707980cbbe3ef254fd876dd5f0b687f3
Don Knapp <don@dknapp.com>
2017-03-24 14:44:28 -0400

    Can click on row or column headers or top left of cal to set/reset

--- app/assets/javascript/calendar.js
+++ app/assets/javascript/calendar.js
@@ -2,9 +2,40 @@
 console.log( "Calendar loaded..." );
 
 $("table").on("click", function(e) {
-    console.log( e.target )
-    setHourCellColor( e.target );
+
+    if ( $(e.target).hasClass( "top-left" ) ) {
+        flipAllHours()
+    } 
+
+    if ( $(e.target).hasClass( "hour" ) ) {
+        flipHourRow(e.target);
+    }
+ 
+    if ( $(e.target).hasClass( "day-head" ) ) {
+        flipDayCellColor( e.target );
+    }
+
+    if ( $( e.target ).hasClass("hour-tag") ) {
+        setHourCellColor( e.target );
+    }
+
 })
+
+function flipDayCellColor( target ) {
+    console.log( target );
+    var day = $(target).attr("data-day-head");
+    $(".hour-tag[data-day='" + day + "']").each( function( i, cell ) {
+        setHourCellColor( cell );
+    })
+}
+
+function flipHourRow(target) {
+    $(target).siblings("td.hour-tag").each( function( i, cell ) { setHourCellColor( cell ) } )
+}
+
+function flipAllHours() {
+    $(".hour-tag").each( function( i, cell ) { setHourCellColor( cell ) } );
+}
 
 function setHourCellColor( target ) {
     if( target.dataset.selected === "0" ) {
@@ -19,5 +50,12 @@ function setHourCellColor( target ) {
 function getSelectedHours() {
     $(".hour-selected").each( function( i, cell ) {
         console.log( i, cell );
+    })
+}
+
+function resetAllhours() {
+    $(".hour-tag").each( function( i, cell ) {
+        $(cell)[0].dataset.selected = "0";
+        $(cell).removeClass("hour-selected");
     })
 }
