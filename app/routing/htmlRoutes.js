6133c4deb4f7268e79c337f4ac85fc6d1afdb8f0
Don Knapp <don@dknapp.com>
2017-03-25 18:41:41 -0400

    Added an additional route to get user and co with email

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,7 +4,8 @@ var stormpath = require( 'express-stormpath' );
 // =============================================================
 module.exports = function(app) {
 
-  app.get("/", stormpath.getUser, function(req, res) {
+  app.get("/", stormpath.loginRequired, function(req, res) {
+    console.log( req.user );
     res.sendFile(path.join(__dirname, "../public/Dashboard/theme/admin_2/app_calendar.html")); //landing page
   });
 
